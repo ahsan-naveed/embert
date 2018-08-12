0d3bd512ff7604c59e648ed244d1e8a381d540ea
Ahsan Naveed <ahsannaveed007@yahoo.com>
2018-08-11 17:49:16 -0700

    3 tests pass

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,20 +1,31 @@
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
+import {visit, currentURL, click} from '@ember/test-helpers';
 
 module('Acceptance | list rentals', function(hooks) {
   //  application is started and shut down between each test
   setupApplicationTest(hooks);
 
   test('should show rentals as the home page', async function(assert) {
-
+    await visit('/');
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
   });
 
   test('should link to information about the company.', async function(assert) {
+    // arrange
+    await visit('/');
 
+    // action
+    await click(".menu-about");
+
+    // assert
+    assert.equal(currentURL(), '/about', 'should navigate to about');
   });
 
   test('should link to contact information.', async function(assert) {
-
+    await visit('/');
+    await click(".menu-contact");
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
   });
 
   test('should list available rentals.', async function(assert) {
